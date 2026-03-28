5cfb73e01f1baf044b736a8b4b2c18d3bdaf16b1
RRTRyan <tiaryanrzk@gmail.com>
2026-03-28 11:24:53 +0300

    initial commit

---
+++ src/main/java/org/rrtryan/springingredients/SpringIngredientsApplication.java
@@ -0,0 +1,13 @@
+package org.rrtryan.springingredients;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringIngredientsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringIngredientsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/rrtryan/springingredients/SpringIngredientsApplicationTests.java
@@ -0,0 +1,13 @@
+package org.rrtryan.springingredients;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringIngredientsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
